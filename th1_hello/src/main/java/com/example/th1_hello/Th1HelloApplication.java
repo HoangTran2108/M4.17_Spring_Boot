a608db8ec1ab191adf311ef48491a4eca782bfc6
HoangTran2108 <119851922+HoangTran2108@users.noreply.github.com>
2023-04-07 00:56:18 +0700

    [Thực hành] Tạo ứng dụng web đơn giản với Spring Boot

---
+++ th1_hello/src/main/java/com/example/th1_hello/Th1HelloApplication.java
@@ -0,0 +1,13 @@
+package com.example.th1_hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Th1HelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Th1HelloApplication.class, args);
+    }
+
+}
---
+++ th1_hello/src/test/java/com/example/th1_hello/Th1HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.th1_hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Th1HelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
